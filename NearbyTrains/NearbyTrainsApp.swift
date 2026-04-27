67257ff154d23ceb51886adb508af4c9dbf314b6
pkj <178080195+peterkjoshi1@users.noreply.github.com>
2026-04-27 14:03:49 +0200

    Initial project

---
+++ NearbyTrains/NearbyTrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyTrainsApp.swift
+//  NearbyTrains
+//
+//  Created by peter on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NearbyTrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
